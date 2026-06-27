f84c0503ceb768d05f3dbfcf659b1bbaf337af68
arina.silenko <291114883+glvgvzh@users.noreply.github.com>
2026-06-27 18:05:11 +0300

    feat: add task creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import "./App.css"
 import { useState } from "react";
-import { FlowerLotusIcon } from "@phosphor-icons/react";
+import { FlowerLotusIcon, XIcon } from "@phosphor-icons/react";
 import Column from "./Column";
 import { columns, tasks as initialTasks } from "./data/boardData";
 
 function App() {
-
     const [tasks, setTasks] = useState(initialTasks)
+    const [isNewTaskModalOpen, setIsNewTaskModalOpen] = useState(false)
+    const [newTaskTitle, setNewTaskTitle] = useState('')
+
+    function handleCreateTask() {
+        if (newTaskTitle.trim() === '') return
+        setTasks(prevTasks => [...prevTasks, {
+            id: Date.now(),
+            status: 'todo',
+            title: newTaskTitle.trim(),
+            description: ''
+        }])
+        setIsNewTaskModalOpen(false)
+        setNewTaskTitle('')
+    }
 
     return (
         <div className="app">
@@ -18,8 +31,41 @@ function App() {
                     className="focus-input"
                     type="text"
                 />
-                <button className="new-task-button">Новая задача</button>
+                <button
+                    className="new-task-button"
+                    onClick={() => setIsNewTaskModalOpen(true)}
+                >
+                    Новая задача
+                </button>
             </div>
+
+            {isNewTaskModalOpen &&
+                <div className="create-new-task-modal-overlay">
+                    <div className="create-new-task-modal">
+                        <input
+                            className="create-new-task-input"
+                            type="text"
+                            value={newTaskTitle}
+                            onChange={e => setNewTaskTitle(e.target.value)}
+                        />
+                        <button
+                            className="create-new-task-button"
+                            onClick={handleCreateTask}
+                        >
+                            Создать
+                        </button>
+                        <button
+                            className="close-modal-button"
+                            aria-label="Закрыть окно создания задачи"
+                            onClick={() => {
+                                setIsNewTaskModalOpen(false)
+                                setNewTaskTitle('')
+                            }}><XIcon />
+                        </button>
+                    </div>
+                </div>
+            }
+
 
             <div className="board">
                 {columns.map(column => {
