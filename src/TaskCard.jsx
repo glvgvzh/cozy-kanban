1f0722e1287195ea2514d3de4c6dbe90b07be932
arina.silenko <291114883+glvgvzh@users.noreply.github.com>
2026-07-09 18:44:26 +0300

    feat: add drag overlay

--- src/TaskCard.jsx
+++ src/TaskCard.jsx
@@ -1,26 +1,16 @@
-import { BookmarkSimpleIcon } from "@phosphor-icons/react"
-import { priorities } from "./data/boardData"
+
 import { useDraggable } from "@dnd-kit/react"
+import TaskCardContent from "./TaskCardContent"
 
 function TaskCard({ task, setSelectedTaskId }) {
-    const priority = priorities.find(priority => priority.id === task.priority)
+    
     const { ref } = useDraggable({
-        id: task.id
+        id: task.id,
     })
 
     return (
-        <div 
-            className="task" 
-            data-priority={task.priority} 
-            onClick={() => setSelectedTaskId(task.id)}
-            ref={ref}>
-            <div className="task-card-info">
-                <div className="task-title">{task.title}</div>
-                <div className="task-description">{task.description}</div>
-                <div className="task-priority">
-                    <BookmarkSimpleIcon size={21} weight="duotone" color={`#${priority.color}`} />{priority.label}
-                </div>
-            </div>
+        <div ref={ref} onClick={() => setSelectedTaskId(task.id)}>
+            <TaskCardContent task={task} />
         </div>
     )
 }
