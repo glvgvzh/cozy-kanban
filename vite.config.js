e5f91cb4c74ede906ae6e72f7975bfded8dbdfd8
arina.silenko <291114883+glvgvzh@users.noreply.github.com>
2026-07-25 17:15:46 +0300

    feat: add offline mode

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
     react(), 
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
       manifest: {
         name: 'Cozy Kanban',
         short_name: 'Kanban',
