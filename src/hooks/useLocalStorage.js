881961633ee98289e177e9184781c9cdd4e889f9
arina.silenko <291114883+glvgvzh@users.noreply.github.com>
2026-08-08 18:37:00 +0300

    refactor: improve local storage handling

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+
+
+function useLocalStorage(key, initialValue) {
+    const [value, setValue] = useState(() => JSON.parse(localStorage.getItem(key)) ?? initialValue)
+    useEffect(() => localStorage.setItem(key, JSON.stringify(value)), [value, key])
+    return [value, setValue]
+}
+
+export default useLocalStorage
