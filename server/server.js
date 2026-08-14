c50a67946a5166d2ecee5b21862db04ca2625cd6
arina.silenko <291114883+glvgvzh@users.noreply.github.com>
2026-08-14 19:03:43 +0300

    feat: add board code verification api

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { getBoardByCode } from './models/board.js'
 
 const server = http.createServer((request, response) => {
     response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
@@ -8,6 +9,26 @@ const server = http.createServer((request, response) => {
         response.end(JSON.stringify({
             status: 'ok',
         }))
+        return
+    }
+
+    if (request.method === 'GET' && request.url.startsWith('/api/boards')) {
+        const urlArray = request.url.split('/')
+        const code = urlArray[urlArray.length - 1]
+        const board = getBoardByCode(code)
+        if (board) {
+            response.statusCode = 200
+            response.setHeader('Content-Type', 'application/json')
+            response.end(JSON.stringify({
+                telegramConnected: true,
+            }))
+        } else {
+            response.statusCode = 404
+            response.setHeader('Content-Type', 'application/json')
+            response.end(JSON.stringify({
+                error: 'Code not found',
+            }))
+        }
         return
     }
 
