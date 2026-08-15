cdfa251418d10a97b4e2bdf938edd06c39e7c540
arina.silenko <291114883+glvgvzh@users.noreply.github.com>
2026-08-15 17:50:09 +0300

    feat: add server task model

---
+++ server/models/task.js
@@ -0,0 +1,64 @@
+import { db } from "../db.js";
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS tasks (
+        id TEXT PRIMARY KEY,
+        board_id INTEGER NOT NULL,
+        status TEXT,
+        title TEXT,
+        description TEXT,
+        created_at INTEGER,
+        priority TEXT,
+        deadline INTEGER
+    )
+`)
+
+const createTaskQuery = db.prepare(`
+    INSERT INTO tasks (id, board_id, status, title, description, created_at, priority, deadline)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+`)
+
+function createTask(task, boardId) {
+    createTaskQuery.run(
+        task.id,
+        boardId,
+        task.status,
+        task.title,
+        task.description,
+        task.createdAt,
+        task.priority,
+        task.deadline
+    )
+}
+
+const deleteTaskByIdQuery = db.prepare(`
+    DELETE FROM tasks WHERE id = ?
+`)
+
+function deleteTaskById(id) {
+    deleteTaskByIdQuery.run(id)
+}
+
+const getTasksByBoardIdQuery = db.prepare(`
+    SELECT * FROM tasks WHERE board_id = ?
+`)
+
+function getTasksByBoardId(boardId) {
+    return getTasksByBoardIdQuery.all(boardId)
+}
+
+const updateTaskByIdQuery = db.prepare(`
+    UPDATE tasks
+    SET status = ?, 
+        title = ?, 
+        description = ?, 
+        priority = ?, 
+        deadline = ?
+    WHERE id = ?
+`)
+
+function updateTaskById(task) {
+    updateTaskByIdQuery.run(task.status, task.title, task.description, task.priority, task.deadline, task.id)
+}
+
+export { createTask, deleteTaskById, getTasksByBoardId, updateTaskById }
